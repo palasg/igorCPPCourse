d2f0febf1b22de8110a5de4b7100eaf452d08eb3
palasg <palas1991@gmail.com>
2022-09-24 10:49:49 +0530

    added couts

--- homeworks/homework_3/guessing_game/guessing_game.cpp
+++ homeworks/homework_3/guessing_game/guessing_game.cpp
@@ -1,1 +1,25 @@
 #include<iostream>
+
+const int generateRandomNum(){
+    return 0;
+}
+
+int main(){
+
+std::cout <<"Welcome to the GUESSING GAME!\n";
+std::cout<<"I will generate a number and you will guess it!\n";
+std::cout <<"Please provide the smallest number: "<<std::endl;
+int smallest_num;
+int largest_num;
+std::cin>> smallest_num;
+
+std::cout <<"Please provide the largest number:"<<std::endl; 
+std::cin>> largest_num;
+
+int randon_num = generateRandomNum();
+
+std::cout << "I've generated a number. Try to guess it!" <<std::endl;
+
+
+    return 0;
+}
