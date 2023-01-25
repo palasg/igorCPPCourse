1487d884d29221681cc8893f4d909d770f53f0b5
palasg <palas1991@gmail.com>
2023-01-25 11:54:42 +0530

    Cmake for utest modified

--- homeworks/homework_4/no_strings_attached/no_strings_attached/split_string_test.cpp
+++ homeworks/homework_4/no_strings_attached/no_strings_attached/split_string_test.cpp
@@ -7,7 +7,7 @@ TEST(Split_string, demo_test){
     ASSERT_EQ(result[1],"World");
     std::vector<std::string> result_1 = no_strings_attached::split_string::Split("Hell o world"," ",1);
     ASSERT_EQ(result_1.size(),1);
-    ASSERT_EQ(result_1[0],"Hello");
+    ASSERT_EQ(result_1[0],"Hell");
 
 
 }
--- homeworks/homework_4/no_strings_attached/no_strings_attached/string_trim_test.cpp
+++ homeworks/homework_4/no_strings_attached/no_strings_attached/string_trim_test.cpp
@@ -6,4 +6,5 @@ TEST(trim_test, demo_trim_test){
     std::string test_str = " Some string with space ";
     char trim_str = ' ';
     std::string some_string = no_strings_attached::string_trim::Trim(test_str,trim_str,no_strings_attached::string_trim::Side::kBoth);
+    ASSERT_EQ(some_string,"Some string with space");
 }
