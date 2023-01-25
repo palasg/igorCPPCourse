fd6380eeeab24b558b0aef62b4e22e378439c1eb
palasg <palas1991@gmail.com>
2023-01-25 11:39:08 +0530

    Trim string utest added

--- homeworks/homework_4/no_strings_attached/examples/trim_strings.cpp
+++ homeworks/homework_4/no_strings_attached/examples/trim_strings.cpp
@@ -3,8 +3,8 @@
 
 
 int main(){
-    std::string str= "This is an exampleTTTTTT";
-    char char_1 = 'T';
+    std::string str= "  his is an example ";
+    char char_1 = ' ';
     std::cout<<"The original string:" <<str<<std::endl;
     std::string after_trim = no_strings_attached::string_trim::Trim(str,char_1,no_strings_attached::string_trim::Side::kBoth);
     std::cout<<"After trim:" <<after_trim<<"\n";
---
+++ homeworks/homework_4/no_strings_attached/no_strings_attached/string_trim_test.cpp
@@ -0,0 +1,9 @@
+#include<gtest/gtest.h>
+#include"inc/string_trim.h"
+
+
+TEST(trim_test, demo_trim_test){
+    std::string test_str = " Some string with space ";
+    char trim_str = ' ';
+    std::string some_string = no_strings_attached::string_trim::Trim(test_str,trim_str,no_strings_attached::string_trim::Side::kBoth);
+}
